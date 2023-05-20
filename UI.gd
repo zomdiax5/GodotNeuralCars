4bd1235d44f454d997d8b777736ba0b084ce1e83
zomdiax5 <45743145+zomdiax5@users.noreply.github.com>
2023-05-20 12:58:06 +0200

    Set Defaults from UI

--- UI.gd
+++ UI.gd
@@ -2,13 +2,24 @@ extends VBoxContainer
 
 func _ready() -> void:
 	Global.connect("next_run",self,"next")
-	$Timescale.connect("value_changed",Global,"setval",["timescale"])                      
+	$Timescale.connect("value_changed",Global,"setval",["timescale"])
+	Global.setval($Timescale.value,"timescale")                     
+	
 	$MutationAmount.connect("value_changed",Global,"setval",["mutation_amount"])                      
+	Global.setval($MutationAmount,"mutation_amount")  
+	
 	$MutationChance.connect("value_changed",Global,"setval",["mutation_chance"])                      
+	Global.setval($MutationChance,"mutation_chance")  
+	
 	$NumberOfAI.connect("value_changed",Global,"setval",["amount_of_AI"])                      
+	Global.setval($NumberOfAI,"amount_of_AI")  
+	
 	$CopyBrain.connect("toggled",Global,"setval",["always_copy_brain"])                      
+	Global.setval($CopyBrain,"always_copy_brain")  
+	
 	$Restart.connect("pressed",Global,"emit_signal",["restart"])
-
+	Global.emit_signal("restart")
+	
 #func _process(delta: float) -> void:
 	#print(Global.timescale)
 
